8bbb7eff5d343cd68559643e91006908a64bcc4b
Christoph Wurst <christoph@winzerhof-wurst.at>
2019-01-23 19:46:14 +0100

    Filter out token logins
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- lib/Listener/LoginListener.php
+++ lib/Listener/LoginListener.php
@@ -61,7 +61,9 @@ class LoginListener {
 		$ip = $this->request->getRemoteAddress();
 		$now = $this->timeFactory->getTime();
 
-		$this->loginClassifier->process($uid, $ip);
+		if (!($data['isTokenLogin'] ?? false)) {
+			$this->loginClassifier->process($uid, $ip);
+		}
 		$this->loginDataCollector->collectSuccessfulLogin($uid, $ip, $now);
 	}
 
