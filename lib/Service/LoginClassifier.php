4b106728d973cc63af49c2081e9a7a5aa7d88b16
Christoph Wurst <christoph@winzerhof-wurst.at>
2019-01-23 13:02:12 +0100

    Check if app password is used by parsing the auth headers
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- lib/Service/LoginClassifier.php
+++ lib/Service/LoginClassifier.php
@@ -25,16 +25,25 @@ declare(strict_types=1);
 
 namespace OCA\SuspiciousLogin\Service;
 
+use function base64_decode;
+use function explode;
+use function preg_match;
+use function strlen;
+use function substr;
 use OCA\SuspiciousLogin\Db\SuspiciousLogin;
 use OCA\SuspiciousLogin\Db\SuspiciousLoginMapper;
 use OCP\AppFramework\Utility\ITimeFactory;
 use OCP\ILogger;
+use OCP\IRequest;
 use Throwable;
 
 class LoginClassifier {
 
 	/** @var EstimatorService */
 	private $estimator;
+
+	/** @var IRequest */
+	private $request;
 
 	/** @var ILogger */
 	private $logger;
@@ -45,17 +54,49 @@ class LoginClassifier {
 	/** @var ITimeFactory */
 	private $timeFactory;
 
+
 	public function __construct(EstimatorService $estimator,
+								IRequest $request,
 								ILogger $logger,
 								SuspiciousLoginMapper $mapper,
 								ITimeFactory $timeFactory) {
 		$this->estimator = $estimator;
+		$this->request = $request;
 		$this->logger = $logger;
 		$this->mapper = $mapper;
 		$this->timeFactory = $timeFactory;
 	}
 
+	/**
+	 * @todo find a more reliable way of checking this
+	 */
+	private function isAuthenticatedWithAppPassword(IRequest $request): bool {
+		$authHeader = $request->getHeader('Authorization');
+		if (is_null($authHeader)) {
+			return false;
+		}
+		if (substr($authHeader, 0, strlen('Basic ')) !== 'Basic ') {
+			return false;
+		}
+		$pwd = explode(
+			':',
+			base64_decode(substr($authHeader, strlen('Basic ')))
+		);
+		if (!isset($pwd[1])) {
+			return false;
+		}
+
+		return preg_match(
+				"/^([0-9A-Za-z]{5})-([0-9A-Za-z]{5})-([0-9A-Za-z]{5})-([0-9A-Za-z]{5})-([0-9A-Za-z]{5})$/",
+				$pwd[1]
+			) === 1;
+	}
+
 	public function process(string $uid, string $ip) {
+		if ($this->isAuthenticatedWithAppPassword($this->request)) {
+			// We don't care about those logins
+			return;
+		}
 		if ($this->estimator->predict($uid, $ip)) {
 			// All good, carry on!
 			return;
