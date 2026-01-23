19dee84cb5c15ae65011b4cc25799131c6b18350
Carl Schwan <carlschwan@kde.org>
2026-01-23 17:41:15 +0100

    chore(psalm): Update to psalm 6
    Signed-off-by: Carl Schwan <carlschwan@kde.org>

--- lib/Migration/Version0Date20181127130604.php
+++ lib/Migration/Version0Date20181127130604.php
@@ -22,6 +22,7 @@ class Version0Date20181127130604 extends SimpleMigrationStep {
 	 * @param array $options
 	 * @return ISchemaWrapper
 	 */
+	#[\Override]
 	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options) {
 		/** @var ISchemaWrapper $schema */
 		$schema = $schemaClosure();
--- lib/Service/Ipv4Strategy.php
+++ lib/Service/Ipv4Strategy.php
@@ -15,38 +15,46 @@ use function array_map;
 use function explode;
 
 class Ipv4Strategy extends AClassificationStrategy {
+	#[\Override]
 	public static function getTypeName(): string {
 		return 'ipv4';
 	}
 
+	#[\Override]
 	public function hasSufficientData(LoginAddressAggregatedMapper $loginAddressMapper, int $validationDays): bool {
 		return $loginAddressMapper->hasSufficientIpV4Data($validationDays);
 	}
 
+	#[\Override]
 	public function findHistoricAndRecent(LoginAddressAggregatedMapper $loginAddressMapper, int $validationThreshold, int $maxAge): array {
 		return $loginAddressMapper->findHistoricAndRecentIpv4($validationThreshold, $maxAge);
 	}
 
+	#[\Override]
 	public function generateRandomIpVector(): array {
 		$ip = $this->generateRandomIp();
 		$splitIp = explode('.', $ip);
 		return $this->numStringsToBitArray($splitIp, 10, 8);
 	}
 
+	#[\Override]
 	protected function ipToVec(string $ip): array {
 		return $this->numStringsToBitArray(explode('.', $ip), 10, 8);
 	}
 
+	#[\Override]
 	public function generateRandomIp(): string {
 		return implode('.', array_map(function (int $index) {
 			return random_int(0, 255);
 		}, range(0, 3)));
 	}
 
+	#[\Override]
 	public function getSize(): int {
 		return 16 + 32;
 	}
 
+	#[\Override]
 	public function getDefaultMlpConfig(): Config {
 		return Config::default();
 	}
